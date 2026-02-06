ffba079b9d7468c25b2511606ed57dcc9cabb9f9
Cruz Prieto Jose Luis <141293124+Joseluiscruz-hub@users.noreply.github.com>
2026-02-06 22:17:58 +0000

    feat: enable Zoneless change detection (provideExperimentalZonelessChangeDetection) in src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideExperimentalZonelessChangeDetection(),
     provideHttpClient(),
     provideAnimations()
   ]
-}).catch((err) => console.error(err));
+}).catch(err => console.error(err));
