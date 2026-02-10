599f4b2b6869864c28af6a03364661d706ba995d
Cruz Prieto Jose Luis <141293124+Joseluiscruz-hub@users.noreply.github.com>
2026-02-09 23:27:44 -0600

    fix: remove zoneless bootstrap

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideExperimentalZonelessChangeDetection } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideExperimentalZonelessChangeDetection(),
     provideHttpClient(),
     provideAnimations()
   ]
