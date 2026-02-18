b2652789539e49705d06b3510f70a7e575e65975
automation <automation@example.com>
2026-02-18 06:37:54 -0600

    feat: agregar soporte PWA (Progressive Web App)

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,13 @@ import { bootstrapApplication } from
 import { AppComponent } from './app.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient(), provideAnimations()]
+  providers: [provideHttpClient(), provideAnimations(), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })]
 }).catch(err => console.error(err));
 // Force rebuild with new hash
